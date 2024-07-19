9a749960a28dc089525a6ad995940db85d48a8e2
ayush-baliyan-19 <ayushbaliyan05@gmail.com>
2024-07-19 17:57:43 +0530

    File Update: put the functions in utils.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,29 +6,7 @@ import { invoices } from "@/lib/constant";
 import { Tablet } from "lucide-react";
 import axios from "axios";
 import { SetStateAction, useEffect, useState } from "react";
-
-export async function getTableData(
-  vs_currency: string,
-  order: string,
-  per_page: number,
-  page: number
-) {
-  const res = await axios.get(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${vs_currency}&order=${order}&per_page=${per_page}&page=${page}&sparkline=false`
-  );
-  return res.data;
-}
-
-export async function getHistoryData(
-  id: string,
-  vs_currency: string,
-  days: number
-) {
-  const res = await axios.get(
-    `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${vs_currency}&days=${days}&precision=0`
-  );
-  return res.data.prices;
-}
+import { getHistoryData, getTableData } from "../lib/utils";
 
 export default function Home() {
   const [dataForTable, setDataForTable] = useState([]);
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import axios from "axios";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function getHistoryData(
+  id: string,
+  vs_currency: string,
+  days: number
+) {
+  const res = await axios.get(
+    `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${vs_currency}&days=${days}&precision=0`
+  );
+  return res.data.prices;
+}
+
+export async function getTableData(
+  vs_currency: string,
+  order: string,
+  per_page: number,
+  page: number
+) {
+  const res = await axios.get(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${vs_currency}&order=${order}&per_page=${per_page}&page=${page}&sparkline=false`
+  );
+  return res.data;
 }
